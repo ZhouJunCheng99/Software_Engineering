d8d1ac3a51c094210985ecb2c2d45a70c60318f5
zchis <2273517548@qq.com>
2024-05-10 01:19:57 -0700

    uesr and admin

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -34,7 +34,7 @@
             立即登录
           </button>
           <div class="tip">
-            默认用户名：admin ，默认密码：123456
+            默认用户名：user ，默认管理员名：admin ，默认密码：123456
           </div>
         </div>
       </div>
@@ -75,7 +75,9 @@ export default {
   components: {},
   data () {
     return {
-      userName: 'admin',
+      // adminName: 'admin',
+      // adminPwd: '123456',
+      userName: 'user',
       userPwd: '123456',
       visible: false,
       modalContent: '这是一段自定义模态框消息'
@@ -94,9 +96,15 @@ export default {
     login () {
       if (this.userName == 'admin' && this.userPwd == '123456') {
         this.$router.push({
-          path: '/index'
+          path: '/Aindex'
         })
-      } else {
+      } 
+      else if (this.userName == 'user' && this.userPwd == '123456'){
+        this.$router.push({
+          path: '/Uindex'
+        })
+      }
+      else {
         this.$Toast({
           content: '请输入正确的用户名和密码',
           type: 'error',
