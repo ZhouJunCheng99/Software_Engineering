1432fe95bf82548511f10ee098f0a39af58cdf40
ZhouJunCheng99 <2112612@mail.nankai.edu.cn>
2024-05-12 11:20:06 +0800

    register finish, frontend and backend mix successfully

--- backend/api/models.py
+++ backend/api/models.py
@@ -7,9 +7,11 @@ class Message(models.Model):
     # body = models.TextField()
     name = models.CharField(max_length=200)
     password = models.TextField()
+    # 权限设置,0表示普通用户,1表示管理员
+    permission = models.IntegerField(default=0)
 
 
 class LoginMessageSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Message
-        fields = ('url', 'name', 'password', 'pk')
+        fields = ('url', 'name', 'password', 'permission', 'pk')
