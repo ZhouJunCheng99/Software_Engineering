b06d67792f06609100c4dbc815eec9d7bb86b3f4
ZhouJunCheng99 <2112612@mail.nankai.edu.cn>
2024-06-28 01:54:11 +0800

    fix some bugs

--- backend/api/apps.py
+++ backend/api/apps.py
@@ -2,4 +2,5 @@ from django.apps import AppConfig
 
 
 class AppConfig(AppConfig):
-    name = 'api'
+    # name = 'api'
+    name = 'backend.api'
--- backend/settings/dev.py
+++ backend/settings/dev.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
+# 需要在Django项目的设置中配置默认的自动字段类型
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 SETTINGS_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BASE_DIR = os.path.dirname(SETTINGS_DIR)
