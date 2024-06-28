c449eaeafcc0458c1c57a74cc7bf1fa60587687b
globe01 <globe_NKU2172@163.com>
2024-06-28 22:14:38 +0800

    add alarm

--- src/views/intelligent/components/ecoLeft.vue
+++ src/views/intelligent/components/ecoLeft.vue
@@ -3,30 +3,40 @@
     <div>
       <dv-border-box-8 :reverse="true" style="padding: 10px">
         <div class="grade">
-          <h2>海洋牧场环境感知得分</h2>
-          <Echart :options="options1" height="350px" />
+          <h2>海洋牧场环境温度感知</h2>
+          <Echart :options="options1" ref="myChart" height="350px" />
         </div>
       </dv-border-box-8>
     </div>
     <div>
-    <dv-border-box-8 :reverse="true">
-      <div class="alarm">
-        <div class="top">
-          <h1>预警</h1>
+      <dv-border-box-8 :reverse="true">
+        <div class="alarm">
+          <div class="top">
+            <h1>预警</h1>
+          </div>
+          <div class="bottom">
+            <h2>水温过高</h2>
+          </div>
         </div>
-        <div class="bottom">
-          <h2>水温过高</h2>
-        </div>
-      </div>
-    </dv-border-box-8>
+      </dv-border-box-8>
     </div>
     <div class="body">
-      <dv-border-box-6  style="padding: 10px">
-        <Echart :options="options2" height="380px"
-      /></dv-border-box-6>
+      <dv-border-box-6 style="padding: 10px">
+        <Echart :options="options2" height="380px" />
+      </dv-border-box-6>
     </div>
   </div>
 </template>
+
+
+
+
+
+
+
+
+
+
 
 <script>
 import Echart from "@/common/echart/index.vue";
@@ -38,130 +48,196 @@ export default {
         series: [
           {
             type: 'gauge',
-            startAngle: 180,
-            endAngle: 0,
-            center: ['50%', '75%'],
-            radius: '90%',
+            center: ['50%', '60%'],
+            startAngle: 200,
+            endAngle: -20,
             min: 0,
-            max: 1,
-            splitNumber: 8,
-            axisLine: {
-              lineStyle: {
-                width: 6,
-                color: [
-                  [0.25, '#FF6E76'],
-                  [0.5, '#FDDD60'],
-                  [0.75, '#58D9F9'],
-                  [1, '#7CFFB2']
-                ]
-              }
+            max: 60,
+            splitNumber: 12,
+            itemStyle: {
+              color: '#FFAB91'
+            },
+            progress: {
+              show: true,
+              width: 30
             },
             pointer: {
-              icon: 'path://M12.8,0.7l12,40.1H0.7L12.8,0.7z',
-              length: '12%',
-              width: 20,
-              offsetCenter: [0, '-60%'],
-              itemStyle: {
-                color: 'auto'
+              show: false
+            },
+            axisLine: {
+              lineStyle: {
+                width: 30
               }
             },
             axisTick: {
-              length: 12,
+              distance: -45,
+              splitNumber: 5,
               lineStyle: {
-                color: 'auto',
-                width: 2
+                width: 2,
+                color: '#999'
               }
             },
             splitLine: {
-              length: 20,
+              distance: -52,
+              length: 14,
               lineStyle: {
-                color: 'auto',
-                width: 5
+                width: 3,
+                color: '#999'
               }
             },
             axisLabel: {
-              color: '#464646',
-              fontSize: 20,
-              distance: -60,
-              rotate: 'tangential',
-              formatter: function (value) {
-                if (value === 0.875) {
-                  return '舒适';
-                } else if (value === 0.625) {
-                  return '正常';
-                } else if (value === 0.375) {
-                  return '隐患';
-                } else if (value === 0.125) {
-                  return '危险';
-                }
-                return '';
-              }
-            },
-            title: {
-              offsetCenter: [0, '-10%'],
+              distance: -20,
+              color: '#999',
               fontSize: 20
             },
+            anchor: {
+              show: false
+            },
+            title: {
+              show: false
+            },
             detail: {
-              fontSize: 30,
-              offsetCenter: [0, '-35%'],
               valueAnimation: true,
-              formatter: function (value) {
-                return Math.round(value * 100) + '';
-              },
+              width: '60%',
+              lineHeight: 40,
+              borderRadius: 8,
+              offsetCenter: [0, '-15%'],
+              fontSize: 30, // 显示温度的字体小点
+              fontWeight: 'bolder',
+              formatter: '{value} °C',
               color: 'inherit'
             },
             data: [
               {
-                value: 0.7,
-                name: '评估得分'
+                value: 20
+              }
+            ]
+          },
+          {
+            type: 'gauge',
+            center: ['50%', '60%'],
+            startAngle: 200,
+            endAngle: -20,
+            min: 0,
+            max: 60,
+            itemStyle: {
+              color: '#FD7347'
+            },
+            progress: {
+              show: true,
+              width: 8
+            },
+            pointer: {
+              show: false
+            },
+            axisLine: {
+              show: false
+            },
+            axisTick: {
+              show: false
+            },
+            splitLine: {
+              show: false
+            },
+            axisLabel: {
+              show: false
+            },
+            detail: {
+              show: false
+            },
+            data: [
+              {
+                value: 20
               }
             ]
           }
-        ],
+        ]
       },
       options2: {
         title: {
           text: "网衣/鱼群检测",
         },
-  tooltip: {
-    trigger: 'axis',
-    axisPointer: {
-      type: 'shadow'
-    }
-  },
-  grid: {
-    left: '3%',
-    right: '4%',
-    bottom: '3%',
-    containLabel: true
-  },
-  xAxis: [
-    {
-      type: 'category',
-      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-      axisTick: {
-        alignWithLabel: true
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'shadow'
+          }
+        },
+        grid: {
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true
+        },
+        xAxis: [
+          {
+            type: 'category',
+            data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+            axisTick: {
+              alignWithLabel: true
+            }
+          }
+        ],
+        yAxis: [
+          {
+            type: 'value'
+          }
+        ],
+        series: [
+          {
+            name: 'Direct',
+            type: 'bar',
+            barWidth: '60%',
+            data: [10, 52, 200, 334, 390, 330, 220]
+          }
+        ]
       }
-    }
-  ],
-  yAxis: [
-    {
-      type: 'value'
-    }
-  ],
-  series: [
-    {
-      name: 'Direct',
-      type: 'bar',
-      barWidth: '60%',
-      data: [10, 52, 200, 334, 390, 330, 220]
-    }
-  ]
-}
     };
   },
+  mounted() {
+    this.updateChart();
+    setInterval(this.updateChart, 10000);
+  },
+  methods: {
+    updateChart() {
+      const random = (Math.random() * 10 + 20).toFixed(2);
+      const color = random > 25 ? 'red' : '#FFC0CB'; // 浅粉色
+      if (random > 25) {
+        alert(`水温过高！已达到 ${random}℃，请尽快处理！`);
+      }
+      this.$refs.myChart.chart.setOption({
+        series: [
+          {
+            data: [
+              {
+                value: random,
+                detail: {
+                  color: color
+                }
+              }
+            ]
+          },
+          {
+            data: [
+              {
+                value: random
+              }
+            ]
+          }
+        ]
+      });
+    }
+  }
 };
 </script>
+
+
+
+
+
+
+
+
 
 <style scoped>
 .content {
@@ -187,7 +263,7 @@ export default {
   margin-top: 10px;
 }
 .alarm {
-  height:80px;
+  height: 80px;
   display: flex;
   flex-direction: column;
   margin-top: 10px;
@@ -208,3 +284,6 @@ h2 {
   text-align: center;
 }
 </style>
+
+
+
